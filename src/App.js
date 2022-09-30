9807669a27d93187e4f2455e3468c32332ad7bc1
Dmitry Tsyplov <dmitry.tsyplov@cibirlan.com>
2022-09-30 13:14:08 +0200

    created input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import {ReactComponent as SearchIcon} from './assets/icons/searchIcon.svg'
+import { ReactComponent as Vector} from './assets/icons/Vector.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      
+    <h1 className='screen_title'>Поиск</h1>
+    <div className='input_container'>
+      <div className='search_icon'><SearchIcon /></div>
+      <div className='vector'><Vector /></div>
+    
+    <input className='input_styles' type="text" placeholder="Введи имя, тег, почту..."/>
+  </div>
+    
+  
     </div>
   );
 }
